3773019e1708e6794cc842e87c3e1fa2413c2c36
Kirill Adamov <highwayman180298@gmail.com>
2026-03-01 21:25:33 +0300

    add atomic and alignas

--- bad_queue.h
+++ bad_queue.h
@@ -18,27 +18,25 @@ public:
     }
     SizeType size()
     {
-        SizeType pop = _pop_cursor.load(std::memory_order_acquire);
-        SizeType push = _push_cursor.load(std::memory_order_acquire);
-        return (push - pop);
+        return (_push_cursor - _pop_cursor);
     }
     bool push(const N& val)
     {
         SizeType push = _push_cursor.load(std::memory_order_relaxed);
-        SizeType pop = _pop_cursor.load(std::memory_order_acquire); //need know exactly what pop is
+        SizeType pop = _pop_cursor.load(std::memory_order_relaxed);
         if (full(push, pop)) return false;
         _arr[push % _capacity] = val;
-        _push_cursor.fetch_add(1, std::memory_order_release); //need tell consumer to update cache
+        _push_cursor.fetch_add(1, std::memory_order_relaxed);
 
         return true;
     }
     bool pop(N& val)
     {
         SizeType pop =  _pop_cursor.load(std::memory_order_relaxed);
-        SizeType push =  _push_cursor.load(std::memory_order_acquire);
+        SizeType push =  _push_cursor.load(std::memory_order_relaxed);
         if (empty(push, pop)) return false;
         val = _arr[pop % _capacity];
-        _pop_cursor.fetch_add(1, std::memory_order_release);
+        _pop_cursor.fetch_add(1, std::memory_order_relaxed);
         return true;
     }
 private:
@@ -58,6 +56,6 @@ private:
 
     N* _arr;
     SizeType _capacity;
-    alignas(64) std::atomic<SizeType> _push_cursor;
-    alignas(64) std::atomic<SizeType> _pop_cursor;
+    std::atomic<SizeType> _push_cursor;
+    std::atomic<SizeType> _pop_cursor;
 };
--- queue.t.cpp
+++ queue.t.cpp
@@ -18,6 +18,7 @@ TEST(QUEUE, seq)
     int result = 0;
     std::thread t1(func_push);
     t1.join();
+    //EXPECT_EQ(queue.size(), iterations);
     int p = 0;
     while (queue.pop(p))
     {
@@ -31,13 +32,12 @@ TEST(QUEUE, concurrent)
     constexpr int iterations_push = 1000000;
     constexpr int iterations_pop = 900000;
 
-    Queue<int> queue(1024);
+    Queue<int> queue(1000000);
     auto func_push = [&queue](){
         int i = 0;
         while(i < iterations_push)
         {
-            queue.push(1);
-            i++;
+            if (queue.push(1)) i++;
         }
     };
 
@@ -46,14 +46,22 @@ TEST(QUEUE, concurrent)
         while(i < iterations_pop)
         {
             int a{};
-            queue.pop(a);
-            i++;
+            if (queue.pop(a)) i++;
+        }
+    };
+    // third for call 
+    auto func_inspector = [&queue]() {
+        for(int i = 0; i < 1000000; ++i) {
+            size_t s = queue.size();
+            if (s > 1000000) abort(); 
         }
     };
     int result = 0;
     std::thread t1(func_push);
     std::thread t2(func_pop);
+    std::thread t3(func_inspector);
     t1.join();
     t2.join();
+    t3.join();
     EXPECT_EQ(queue.size(), iterations_push - iterations_pop);
 }
